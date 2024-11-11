227f185345306c1d9674a6457b76c12c52efec1f
Peter Todorov <peter.todorov@live.com>
2024-11-11 20:25:46 +0000

    Remove extra lines from 'dataset.py'

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,4 @@
 import random
-from glob import glob
-import numpy as np
 from torch.utils.data import Dataset
 import torchaudio
 import torchaudio.transforms as T
@@ -12,29 +10,6 @@ import matplotlib.pyplot as plt
 def readJson(json_path):
     with open(json_path, "r") as f:
         return json.load(f)
-
-
-class ImageDataset(Dataset):
-    def __init__(
-            self, input_path, target_path=None, transform=None,
-            input_normalize=None):
-        self.input_audio_paths = readAudioPaths(input_path)
-        self.target_occupancy_paths = readCsvPaths(target_path)
-        self.transform = transform
-        self.input_normalize = input_normalize
-
-    def __len__(self):
-        return len(self.input_audio_paths)
-
-    def __getitem__(self, i):
-        seed = random.randint(0, 2**32)
-        input_image = readAudio(self.input_audio_paths[i], self.transform, seed)
-        if self.input_normalize:
-            input_image = self.input_normalize(input_image)
-        if self.target_occupancy_paths is not None:
-            target_image = readAudio(self.target_occupancy_paths[i], self.transform, seed)
-            return input_image, target_image
-        return input_image, self.input_audio_paths[i]
 
 
 class AudioSpectrogramDataset(Dataset):
