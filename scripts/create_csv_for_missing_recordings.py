1c17d4f2d9416163982ce9a1f0e380659dce46fe
Peter Todorov <peter.todorov@live.com>
2024-11-13 17:43:25 +0000

    Create missing CSV file for a recording

---
+++ scripts/create_csv_for_missing_recordings.py
@@ -0,0 +1,34 @@
+import csv
+from datetime import datetime
+import os
+
+
+WAV_FILE_PATH = "/Users/todorov/Documents/ef/occupancy-ml/20241016_155159.WAV"
+OCCUPANCY = WAV_FILE_PATH.split("/")[-2].split("_")[0]
+
+
+def extractAndFormatTimestamp(filename):
+    if filename.endswith(".WAV"):
+        filename = filename[:-4]
+    date_str, time_str = os.path.basename(filename).split("_")  # "YYYYMMDD_HHMMSS"
+    timestamp = datetime.strptime(date_str + time_str, "%Y%m%d%H%M%S")
+    unix_seconds = float(timestamp.timestamp())
+    formatted_time = timestamp.strftime("%Y-%m-%d_%H:%M:%S")
+    return formatted_time, unix_seconds
+
+
+def writeToCsv(wav_filename):
+    csv_filename = os.path.splitext(wav_filename)[0] + ".csv"
+    formatted_time, unix_seconds = extractAndFormatTimestamp(wav_filename)
+    data_to_add = [
+        ["Time", "Seconds", "Seconds-from-start", "Count"],
+        [formatted_time, f"{unix_seconds:.2f}", 0.0, OCCUPANCY]
+    ]
+    with open(csv_filename, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(data_to_add)    
+    print(f"CSV file created: '{csv_filename}'")
+
+
+if __name__ == "__main__":
+    writeToCsv(WAV_FILE_PATH)
