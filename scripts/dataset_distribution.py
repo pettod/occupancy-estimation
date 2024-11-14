a34ba863711a6e40813dbb7310bd8c7720e91890
Peter Todorov <peter.todorov@live.com>
2024-11-14 11:07:56 +0000

    Center occupancy distribution ticks

--- scripts/dataset_distribution.py
+++ scripts/dataset_distribution.py
@@ -5,24 +5,18 @@ import matplotlib.pyplot as plt
 train_file = "dataset_2-0s_train.json"
 valid_file = "dataset_2-0s_valid.json"
 
-# Step 1: Read the JSON data
+# Read data
 with open(train_file, "r") as file:
     data = json.load(file)
 with open(valid_file, "r") as file:
     data += json.load(file)
 occupancies = [entry["occupancy"] for entry in data]
 
-# Step 3: Plot the histogram of counts
-bins = range(min(occupancies), max(occupancies) + 2)
-hist_values, bins, _ = plt.hist(occupancies, bins=bins, edgecolor="black")
-
-# Step 4: Center-align ticks by setting them to the middle of each bin
-bin_centers = [(bins[i] + bins[i + 1]) / 2 for i in range(len(bins) - 1)]
-plt.xticks(bin_centers, labels=[str(int(bin)) for bin in bins[:-1]])
+# Plot histogram of counts
+bins = range(min(occupancies), max(occupancies) + 5)
+plt.hist(occupancies, bins=bins, edgecolor="black", align="left")
 
 plt.xlabel("Occupancy count")
 plt.ylabel("Number of samples")
 plt.title("Occupancy distribution")
-
-# Show the plot
 plt.show()
