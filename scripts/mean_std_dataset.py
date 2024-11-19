0f9337e1890bb50228e661a937a2b015a7927e96
Peter Todorov <peter.todorov@live.com>
2024-11-19 16:06:31 +0000

    Calculate mean and STD properly

--- scripts/mean_std_dataset.py
+++ scripts/mean_std_dataset.py
@@ -1,20 +1,25 @@
 from src.dataset import AudioSpectrogramDataset as Dataset
 import torch
 from tqdm import tqdm
+from config import CONFIG
 
 
-TRAIN_FILE = "dataset_2-0s_train.json"
+TRAIN_FILE = "dataset_60-0s_train.json"
 REPLACED_DATA_PATH_ROOT = "data_high-pass"
-TRAIN_DATASET = Dataset(TRAIN_FILE, REPLACED_DATA_PATH_ROOT)
+TRAIN_DATASET = Dataset(
+    TRAIN_FILE,
+    REPLACED_DATA_PATH_ROOT,
+    transform=CONFIG.TRANSFORM,
+)
 
 
 def main():
-    mean = 0
-    std = []
+    samples = []
     for spectrogram, occupancy in tqdm(TRAIN_DATASET):
-        mean += torch.mean(spectrogram)
-        std.append(spectrogram)
-    std = torch.std(torch.stack(std))
+        samples.append(spectrogram)
+    samples = torch.stack(samples)
+    std = torch.std(samples)
+    mean = torch.mean(samples)
     print("Mean:", mean)
     print("Std:", std)
 
